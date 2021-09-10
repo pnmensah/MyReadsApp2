f817432cda1f9b257d97a32e593506cf4025c7de
Paul Mensah <pnmensah@live.com>
2021-09-10 22:34:26 +0000

    Update on book shelf state

--- src/components/Search.js
+++ src/components/Search.js
@@ -24,6 +24,8 @@ class Search extends Component {
   handleChange = (id, e) => {
     const shelf = e.target.value;
     this.props.setBookCategory(shelf, id);
+    value={book.shelf || "none"}
+    
   };
 
   render() {
